bc59a581680c972f768f0057d84a7d31aa181c39
Abayomi Koyejo <AbayomiKoye@gmail.com>
2022-03-24 16:37:51 +0300

    updated

---
+++ learn.ps1
@@ -0,0 +1,13 @@
+get-help g*service*
+get-hel *services*
+
+gal gsv
+gal 
+
+Get-Verb
+Get-Help gsv -Examples
+Get-Help gsv -Detailed
+Get-Help gsv -ShowWindow
+
+Get-Service "s*" | Sort-Object status
+
